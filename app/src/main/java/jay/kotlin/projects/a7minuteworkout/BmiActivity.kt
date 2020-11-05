04c201d3dcbadf5b2326d5155a0941798e08f0fe
jaysharma8-mca <jaysharma155.cmpica@gmail.com>
2020-11-05 22:03:05 +0530

    added bmi calculation formula

--- app/src/main/java/jay/kotlin/projects/a7minuteworkout/BmiActivity.kt
+++ app/src/main/java/jay/kotlin/projects/a7minuteworkout/BmiActivity.kt
@@ -3,6 +3,8 @@ package jay.kotlin.projects.a7minuteworkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_bmi.*
 
 class BmiActivity : AppCompatActivity() {
@@ -19,10 +21,21 @@ class BmiActivity : AppCompatActivity() {
         }
     }
 
+    fun calculateBmi(view: View) {
+        val weight = etMetricUnitWeight.text.toString()
+        val height = etMetricUnitHeight.text.toString()
+        val heightInMeter = height.toFloat()/100
+
+        val calculatedBMI = weight.toInt() / (heightInMeter * heightInMeter)
+        Toast.makeText(this,calculatedBMI.toString(),Toast.LENGTH_SHORT).show()
+    }
+
     override fun onBackPressed() {
         super.onBackPressed()
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finishAffinity()
     }
+
+
 }
